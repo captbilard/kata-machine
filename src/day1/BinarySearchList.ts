20f14ecfc7cb3d8f61e53494ec2599a800062cbc
bilard <bilardosinulu@gmail.com>
2023-03-01 17:59:33 -0700

    Binary search List

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,21 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    /**
+     * We set the pointers to both end of the array lo & hi, then we get the mid point(m) and check
+     * if it's the value we want, if it's not then we check the size, if it's > the needle
+     * we set the high pointer to m-1 else we set the lo pointer to m+1, all these is done
+     * while lo < hi
+     */
+    let lo = 0;
+    let h = haystack.length;
+    do {
+        let m = Math.floor(lo + (h - lo) / 2);
+        if (haystack[m] === needle) {
+            return true;
+        } else if (haystack[m] > needle) {
+            h = m - 1;
+        } else {
+            lo = m + 1;
+        }
+    } while (lo < h);
+    return false;
 }
